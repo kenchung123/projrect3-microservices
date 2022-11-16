a9d8aa37f55c0184bf62ea9bac8efede55d643c9
chungvt <kenchung170798@gmail.com>
2022-11-16 21:38:47 +0800

    update code for deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a02339f8b7f3941af90a4142acf54213-217206021.us-west-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
